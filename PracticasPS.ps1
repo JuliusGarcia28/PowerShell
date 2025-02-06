53c3200575caa077ffc7561dc9cf66f26c3f6108
JuliusGarcia28 <julianjaviergarciaalvarez@gmail.com>
2025-02-06 15:37:48 -0700

    Update PracticasPS.ps1
    Actualizacion del archivo que contiene todos los scripts de las Practicas de PowerShell.

--- PracticasPS.ps1
+++ PracticasPS.ps1
@@ -263,4 +263,133 @@ fuction Get-Resta{
     $comando.Connection.Close()
  }
 
-  <#Me quede en la pagina 72#>
+  try{
+    Start-Something -Path $path -ErrorAction Stop
+  }catch [System.IO.DirectoryNotFoundException],[System.IO.FileNotFoundException]{
+    Write-Output "El directorio o fichero no ha sido encontrado:[$path]"
+  }catch [System.IO.IOException]{
+    Write-Output "Error de IO con el archivo: [$path]"
+  }
+
+  throw "No se puede encontrar la ruta: [$path]"
+
+  throw [System.IO.FileNotFoundException] "No se puede encontrar la ruta: [$path]"
+
+  throw [System.IO.FileNotFoundException]::new()
+
+  throw [System.IO.FileNotFoundException]::new("No se puede encontrar la ruta: [$path]")
+
+  throw (New-Object -TypeName System.IO.FileNotFoundException)
+
+  throw (New-Object -TypeName System.IO.FileNotFoundException -ArgumentList "No se puede encontrar la ruta: [$path]")
+
+trap{
+    Write-Output $PSItem.ToString()
+}
+throw [System.Exception]::new('primero')
+throw [System.Exception]::new('segundo')
+throw [System.Exception]::new('tercero')
+
+<#Backup en ruta especifica#>
+fuction Backup-Registry {
+    Param(
+        [Parameter(Mandatory = $true)]
+        [string]$rutaBackup
+    )
+
+    #Crear la ruta de ddestino del backup si no existe
+    if (!(Test-Path -Path $rutaBackup)){
+        New-Item -ItemType Directory -Path $rutaBackup | Out-Null
+    }
+
+    #Generar un nombre unico para el archivo de backup si no existe
+    $nombreArchivo = "Backup-Registry_" + (Get-Date -Format "yyyy-MM-dd-mm-ss") + ".reg"
+    $rutaArchivo = Join-Path -Path $rutaBackup -ChildPath
+    $nombreArchivo
+
+    #Realizar el backup del registro del sistema y guardarlo en archivo de destino
+    try{
+        Write-Host "Realizando backup del registro del sistema en $rutaArchivo..."
+        reg export HKLM $rutaArchivo
+        Write-Host "El backup del registro del sistema se ha realizado con exito"   
+    }catch{
+        Write-Host "Se ha producido un error al realizar un backup del registro del sistema: $_"
+    }
+}
+
+<#Backup funciones agregadas#>
+#Escribir el codigo en archivo log
+$logDDirectory = "$env:APPDATA\RegistryBackup"
+$logFile = Join-Path $logDirectory "backup-registry_log.txt"
+logEntry = "$(Get-Date) - $env:USERNAME - Backup - $backupPath"
+if(!(Test-Path $logDirectory)) {
+    New-Item -ItemType Directory -Path $logDirectory | Out-Null
+}
+Add-Content -Path $logFile -Value $logEntry
+
+#Verificar si hay mas $backupCount backups en el directorio y eliminar los mas antiguos si es necesario
+$backupCount = 10
+$backups = Get-ChildItem $backupDirectory -Filter *.reg | Sort-Object LastWriteTime -Descending
+if ($backups.Count -gt $backupCount) {
+    backupsToDelete = $backups[$backupCount..($backups.Count-1)]
+    $backupsToDelete | Remove-Item -Force
+}
+
+#Creacion de script que crea un archivo .psd1
+@{
+    ModuleVersion = '1.0.0'
+    PowerShellVersion = '5.1'
+    RootModule = 'Backup-Registry.ps1'
+    Description = 'Modulo para realizar backups del registro del sistema de windows'
+    Autho = 'Julian'
+    FunctionsToExport = @('Backup-Registry')
+}
+
+#Automatizacion de la ejecucion del backup
+#Configuracion de la tarea
+$Time = New-ScheduledTaskTrigger -At 02:00 -Daily
+#Atencion de la tarea
+$PS = New-ScheduledTaskAction -Excute "Powershell.exe" -Argument
+"-Command '"Import-Module BackupRegistry -Force; backup-Registry -rutaBackup 'D:\tmp\Backups\Registro'"'"
+#Crear la tarea programada
+Register-ScheduledTask -TaskName "Ejecutar Backupdel Registro del sistema" -Trigger $Time -Action $PS
+
+<#Administracion con PowerShell#>
+Get-Service
+
+Stop-Service -Name Spooler -Confirm -PassThru
+
+Start-Service -Name Spooler -Confirm -PassThru
+
+Suspend-Service -Name Spooler -Confirm -PassThru
+
+Restart-Service -Name WSearch -Confirm -PassThru
+
+Set-Service -Name dcsvc -DisplayName "Servicio de virtualizacion de credenciales de seguridad distribuidas"
+
+Get-CimInstance Win32_Service -Filter 'Name = "BITS"' | Format-List Name, Description
+
+Get-Process -Name Acrobat
+
+Wait-Process -Name Notepad
+
+<#Administracion de usuarios y grupos#>
+Get-LocalUser
+
+Get-LocalUser -Name Julius | Select-Object *
+
+Get-LocalGroup
+
+Get-LocalGroup -Name Administradores | Select-Object *
+
+New-LocalUser -Name "Usuario1" -Desccripcion "Usuario de prueba 1" -NoPassword
+
+Get-LocalUSer -Name "Usuario1"
+
+Remove-LocalUser -Name "Usuario1"
+
+New-LocalGroup -Name 'Grupo' -Descripcion 'Grupo de prueba 1'
+
+Add-LocalGroupMember -Group Grupo1 -Member Usuario2 -Verbose
+
+Remove-LocalGroupMember -Group Grupo1 -Member Usuario1
